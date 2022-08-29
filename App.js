ceda26be3b1094f936d0325a424ef15f78943a4d
Shubham Salunke <shubhamdeepak007@gmail.com>
2022-08-29 20:06:27 +0530

    Switched to RN Elements Added SignIn Screen

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 import Intro from "./src/pages/Intro";
 import styles from "./src/config/styles";
 import {useFonts} from "expo-font";
-import { ActivityIndicator, MD2Colors as Colors } from 'react-native-paper';
+import {useState} from "react";
+import SignIn from "./src/pages/SignIn";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+import Home from "./src/pages/Home";
+import SignUp from "./src/pages/SignUp";
 
+
+function Main() {
+    const Stack = createNativeStackNavigator();
+    return (
+        <NavigationContainer>
+            <Stack.Navigator  initialRouteName="SignIn" screenOptions={{headerShown: false}}>
+                <Stack.Screen name="SignIn" component={SignIn} />
+                <Stack.Screen name="SignUp" component={SignUp} />
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
 
 export default function App() {
+
+    const [done, setDone] = useState(false);
 
     const [fonts] = useFonts({
         'Raleway': require('./assets/Raleway-SemiBold.ttf'),
@@ -16,14 +36,14 @@ export default function App() {
 
     if (!fonts) {
         return (
-            <ActivityIndicator animating={true} colors={Colors.blue300} />
+            <ActivityIndicator animating={true} />
         );
     }
 
     return (
         <View style={styles.container}>
             <StatusBar style="auto" translucent={true}/>
-            <Intro />
+            {done ? <Main/> : <Intro setDone={setDone}/>}
         </View>
     );
 }
--- src/components/SafeView.jsx
+++ src/components/SafeView.jsx
@@ -5,7 +5,7 @@ import styles from "../config/styles";
 
 const SafeView = props => {
     return (
-        <View style={{...styles.globalStatusBar, ...props.style}}>
+        <View style={{...styles.globalStatusBar, backgroundColor: '#ffffff', height: '100%', ...props.style}}>
             {props.children}
         </View>
     );
